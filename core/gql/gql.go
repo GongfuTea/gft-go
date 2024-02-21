2b89993a455a2ad8d57c9db2927e8ce9bd14136c
pswang <pengshengwang@gmail.com>
2024-02-21 16:19:46 +0800

    清理代码

--- core/gql/gql.go
+++ core/gql/gql.go
@@ -1,10 +1,8 @@
 package gql
 
 import (
-	"encoding/json"
 	"errors"
 
-	"github.com/GongfuTea/gft-go/types"
 	"github.com/graphql-go/graphql"
 )
 
@@ -16,36 +14,36 @@ func GqlMustLogin(p graphql.ResolveParams) {
 
 }
 
-func GqlParse[T any](val any, input T) (output T, err error) {
-	data, _ := json.Marshal(val)
-	err = json.Unmarshal(data, &input)
-	return input, err
-}
+// func GqlParse[T any](val any, input T) (output T, err error) {
+// 	data, _ := json.Marshal(val)
+// 	err = json.Unmarshal(data, &input)
+// 	return input, err
+// }
 
-func GqlParseInput[T types.IEntity](p graphql.ResolveParams, input T) (output T, err error) {
-	args := p.Args["input"].(map[string]any)
-	data, _ := json.Marshal(args)
-	// fmt.Printf("save category, %+v", args)
+// func GqlParseInput[T types.IEntity](p graphql.ResolveParams, input T) (output T, err error) {
+// 	args := p.Args["input"].(map[string]any)
+// 	data, _ := json.Marshal(args)
+// 	// fmt.Printf("save category, %+v", args)
 
-	err = json.Unmarshal(data, &input)
-	return input, err
-}
+// 	err = json.Unmarshal(data, &input)
+// 	return input, err
+// }
 
-func GqlParseFilter[T any](p graphql.ResolveParams, input T) (output T, err error) {
-	args := p.Args["filter"]
-	data, _ := json.Marshal(args)
-	// fmt.Printf("save category, %+v", args)
+// func GqlParseFilter[T any](p graphql.ResolveParams, input T) (output T, err error) {
+// 	args := p.Args["filter"]
+// 	data, _ := json.Marshal(args)
+// 	// fmt.Printf("save category, %+v", args)
 
-	err = json.Unmarshal(data, &input)
-	return input, err
-}
+// 	err = json.Unmarshal(data, &input)
+// 	return input, err
+// }
 
-func MergeFields(list ...graphql.Fields) graphql.Fields {
-	var fields = graphql.Fields{}
-	for _, items := range list {
-		for k, v := range items {
-			fields[k] = v
-		}
-	}
-	return fields
-}
+// func MergeFields(list ...graphql.Fields) graphql.Fields {
+// 	var fields = graphql.Fields{}
+// 	for _, items := range list {
+// 		for k, v := range items {
+// 			fields[k] = v
+// 		}
+// 	}
+// 	return fields
+// }
